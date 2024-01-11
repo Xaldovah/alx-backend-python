455291e2e45a4008c4160c56df0d118454266c3d
xaldovah <dxaldovah@gmail.com>
2024-01-11 20:56:48 +0300

    Use mypy to validate the following piece of code and apply any necessary changes

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""A module to demonstrate type checking using mypy."""
+
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Zoom in on the elements of a tuple.
+
+    Args:
+        lst (Tuple): The input tuple.
+        factor (int, optional): The zoom factor. Defaults to 2.
+
+        Returns:
+            Tuple: contains the elems of input tuple zoomed in based on factor
+    """
+    zoomed_in: List = [
+            item for item in lst
+            for i in range(factor)
+    ]
+    return tuple(zoomed_in)
+
+
+array = (12, 72, 91)
+
+
+zoom_2x = zoom_array(array)
+
+
+zoom_3x = zoom_array(array, 3)
