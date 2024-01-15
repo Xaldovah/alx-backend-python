97a459d07305c418998da24760a284a14d4cb540
xaldovah <dxaldovah@gmail.com>
2024-01-15 18:07:22 +0300

    execute multiple coroutines at the same time with async

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+This module async func spawns wait_random n times with the specified max_delay
+"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Async routine that spawns task_wait_random n times
+    with the specified max_delay.
+
+    Args:
+        n (int): Number of times to spawn task_wait_random.
+        max_delay (int): The maximum delay value.
+
+        Returns:
+            List[float]: List of delays in ascending order.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
