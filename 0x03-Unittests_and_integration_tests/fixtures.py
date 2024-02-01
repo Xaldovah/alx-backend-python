1525710df1f7872fa834d3e188985b3499a05364
xaldovah <dxaldovah@gmail.com>
2024-02-01 17:03:01 +0300

    Unittests and integration tests

---
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+TEST_PAYLOAD = [
+        (
+            {"repos_url": "https://api.github.com/orgs/google/repos"},
+            [
+                {
+                    "id": 7697149,
+                    "node_id": "MDEwOlJlcG9zaXRvcnk3Njk3MTQ5",
+                    "name": "episodes.dart",
+                    "full_name": "google/episodes.dart",
+                    "private": False,
+                    "owner": {
