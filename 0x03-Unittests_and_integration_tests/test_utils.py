c4cbbbed9a16485c120047d46f72b4edbb23f17b
xaldovah <dxaldovah@gmail.com>
2024-02-04 23:42:33 +0300

    test that the method returns what it is supposed to

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Module function that inherits from unittest.TestCase"""
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    This class uses parameterized.expand to allow
+    multiple sets of inputs and their corresponding
+    expected results
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """
+        Test the access_nested_map function with various inputs.
+
+        Parameters:
+            - nested_map: A nested map.
+            - path: A sequence of keys representing a path to the value.
+            - expected_result: The expected result when accessing the
+            nested map.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
