a61f629b0ca5e063661af1bbdc9f941b0996835c
xaldovah <dxaldovah@gmail.com>
2025-01-05 23:56:14 +0300

    Implement Authentication

---
+++ messaging_app/messaging_app/settings.py
@@ -0,0 +1,25 @@
+from datetime import timedelta
+
+INSTALLED_APPS += [
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',  # For blacklisting tokens if needed
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'ALGORITHM': 'HS256',
+}
